925408ebbf91ea7d8c08e97a0c579a73d24f3344
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-09-29 08:43:56 -0400

    Added normal&Javadoc comments, edited solutions

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,25 +1,21 @@
 package gr11review.part2;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Utility {
-    public static void main(String[] args) {
-        // All test cases
-        
+    public static void main(String[] args)throws IOException {
         xyBalance("xaaby");
-        //longestWord("C:/Users/gavin/OneDrive/Desktop/grade-11-review-2---methods-and-testing-gavin-alex/src/gr11review/test2/Review2_3Test_1.txt");
+        longestWord("C:\\Users\\gavin\\OneDrive\\Desktop\\grade-11-review-2---methods-and-testing-gavin-alex\\src\\gr11review\\test2\\Review2_3Test_1.txt");
         int [] array = {1, 10, 10, 2};
         withoutTen(array);
         int [] arrayTwo = {1, 10, 10, 2};
         canBalance(arrayTwo);
         int [][] arrayThree = {{1,2,3},{4,5,6},{7,8,9}};
         split(arrayThree, 2, 2);
-
     }
 
     /**
-     * Given a string determine if it's x y balanced
+     * Given a string determine if it's x-y balanced
      * 
      * @param str Given a string determine if it's x and y balanced
      * @return if the string is x-y balanced
@@ -27,20 +23,18 @@ public class Utility {
      * 
      */
 
-
     public static boolean xyBalance(String str) {
 
         // Initialize variables for location of x and y chars
         int intX = 0;
         int intY = 0;
 
-
         // If index of y is greater than x, string is balanced
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == 'x'){
+        for (int i = 1; i < (str.length()) + 1; i++){
+            if (str.charAt(i - 1) == 'x'){
                 intX = i;
             }
-            if (str.charAt(i) == 'y'){
+            if (str.charAt(i - 1) == 'y'){
                 intY = i;
             }
             if (intY > intX){
@@ -50,27 +44,45 @@ public class Utility {
         }
         // Otherwise, return false.
         System.out.println("False");
-        return false;
-        
+        if (intX == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    
-    public static String longestWord(String filenametxt)throws FileNotFoundException{
+
+    /**
+     * 
+     * Given a file with multiple lines of words, return the longest word
+     * 
+     * @param filenametxt A given text file
+     * @return return the longest word as a string
+     * @author G.Ge
+     * 
+     */
+
+    public static String longestWord(String filenametxt)throws IOException{
         String strLargest = "";
         String strCurrent;
-        Scanner scanner  = new Scanner(new File(filenametxt));
-        String line;
-        while (scanner.hasNextLine()) {
-            strCurrent = scanner.nextLine();
+        BufferedReader reader = new BufferedReader(new FileReader(filenametxt));
+        String line = reader.readLine();
+        while (line != null) {
+            strCurrent = line;
             if (strCurrent.length() > strLargest.length()){
                 strLargest = strCurrent;
             }
+            line = reader.readLine();
+            System.out.print(strCurrent);
         }
+        reader.close();
         return strLargest;
     }
     
     
     /**
-     * Given an array replace all '10s' with 0s at the end of the array and move all nonten elements to the beginning
+     * 
+     * Given an array replace all '10s' with 0s at the end of the array and move all non-ten elements to the beginning
      * 
      * @param nums array of numbers to sort 
      * @return array with tens replaced 
@@ -97,32 +109,37 @@ public class Utility {
     }
    
     /**
+     * 
      * Given an array determine if you can split the array, so boths sides are equal
      * 
      * @param nums Array to be split
      * @return If the array could be split equally 
      * @author G.Ge
+     * 
      */
+
     public static boolean canBalance(int[] nums) {
         // Variables for the sumes of the array from left and right
         int intSumLeft = 0;
         int intSumRight = 0;
         
         for(int i = 0; i < nums.length; i++){
+            intSumRight = 0;
             intSumLeft += nums[i];
-            for (int x = nums.length-1; x>i; x--){
+            for (int x = (nums.length-1); x>i; x--){
                 intSumRight += nums[x];
                 if (intSumLeft == intSumRight && x == (i+1)){
                     System.out.println("true");
                     return true;
                 }
-                intSumRight = 0;
             }
         }
         System.out.println("false");
         return false;
     }
+
     /**
+     * 
      * Given an 2d array, return an array with elements up to a certain row and column
      * 
      * @param arr Original array
@@ -130,7 +147,9 @@ public class Utility {
      * @param col The column to fetch up to
      * @return an array with elements up to the designated row and column
      * @author G.Ge
+     * 
      */
+
     public static int[][] split(int[][] arr, int row, int col){
 
         // Create new array up to length of row and column desured
@@ -144,10 +163,5 @@ public class Utility {
             }
         }
         return newArray;
-        
     }
-
-    
-
 }
-
