787108d53b903ce05719612f8e22e003c7065322
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-09-29 21:49:36 -0400

    Complete methods 3

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,8 +1,35 @@
 package gr11review.part2;
 
+import java.io.*;
+
 public class Utility {
 
-    
 
+    public static void main(String[] args)throws IOException {
+
+        System.out.println(xyzMiddle("AAxyzBB"));
+        System.out.println(xyzMiddle("AxyzBB"));
+        System.out.println(xyzMiddle("AxyzBBB"));
     
+    }
+
+
+    public static boolean xyzMiddle(String str) {
+        int middle = str.length()/2 - 1;
+
+        // check right of middle
+        if (str.length() >= 3 && (str.substring(middle, middle + 3).equals("xyz"))) {
+            return true;
+        }
+        
+        // check left of middle
+        if (str.length() % 2 == 0 && str.substring(middle - 1, middle + 2).equals("xyz")) {
+            return true;
+         }
+
+        // return false if xyz is not found in the middle of the string
+        return false;
+    }
+
+
 }
